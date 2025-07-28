4e6d73755272f26d13a21823d686c173777cbaef
rohit2195-jpg <rsattulu@purdue.edu>
2025-07-28 16:31:17 -0400

    render setup

--- backend/backend.py
+++ backend/backend.py
@@ -529,5 +529,5 @@ def download_transactions():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded =True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), threaded=True)
 
